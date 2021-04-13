98d7896b6bb20aa20694e8c7f3b3da722811e727
sahilsingh-jubbal <63543000+sahilsingh-jubbal@users.noreply.github.com>
2021-04-13 18:32:12 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+// 'mongodb://127.0.0.1:27017/task-manager-api'
+mongoose.connect(MONGOOB_URL , {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
